d311e844377a1555b0cc2c2ea38d21c1fe0f3e9d
islamgouda <33799849+islamgouda@users.noreply.github.com>
2024-09-21 01:52:17 +0300

    Add project files.

---
+++ WebDavSharp.App/Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Builder;
+using WebDavSharp.Core;
+using WebDavSharp.Core.WebDAV;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Http;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.Configure<WebDavOptions>(builder.Configuration.GetSection("General"));
+var c = builder.Configuration.GetSection("Filesystem");
+builder.Services.Configure<LocalFilesystemOptions>(builder.Configuration.GetSection("Filesystem"));
+
+builder.Services.AddWebDavSharp(x => x.IsReadOnly = false)
+                .AddFilesystem();
+
+var app = builder.Build();
+
+var env = app.Services.GetRequiredService<IWebHostEnvironment>();
+
+if (env.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpsRedirection();
+app.UseWebDavSharp(PathString.Empty);
+
+app.Run();
---
+++ WebDavSharp.Core/Filesystems/LocalFilesystems/LocalFilesystemOptions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace WebDavSharp.Core
+{
+    public class LocalFilesystemOptions
+    {
+        public LocalFilesystemOptions()
+        {
+            SourcePath = "";
+        }
+
+        public string SourcePath { get; set; }
+    }
+}
